4741a85a9ab891d1c218d95dd80d5364a38d91c1
Oleksii Piliai <alekseivpiliai@gmail.com>
2024-12-08 16:47:37 +0100

    start2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Profile from "./components/profile/Profile"
+import Profile from "./components/Profile/Profile"
 import userData from "./userData.json"
 import FriendListItem from "./components/FriendList/FriendList";
 import friends from "./friends.json"
